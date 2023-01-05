64114fe6c2fe3ae5f096f55d1c36ccf37c6bc1d4
Bhushan Bharat Borude <110595758+bhushan-b-borude@users.noreply.github.com>
2023-01-05 14:26:49 +0100

    Update penguins_app.py

--- penguins_app.py
+++ penguins_app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import pickle
-from sklearn.ensemble import RandomForestClassifier
+from scikit-learn.ensemble import RandomForestClassifier
 from PIL import Image
 
 
